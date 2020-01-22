e02abdac965ebb80f6494da4c53c893dde8c51d3
Marius Rak <rak.marius@gmail.com>
2020-01-22 18:19:11 +0100

    Initial

---
+++ client/main.js
@@ -0,0 +1,19 @@
+// meteor create . --minimal
+// npm install react react-dom
+
+
+import { Meteor } from 'meteor/meteor';
+import React from 'react';
+import { render } from 'react-dom';
+
+class HelloWorld extends React.Component {
+  render() {
+    return (
+      <h1>Hello World</h1>
+    );
+  }
+}
+
+Meteor.startup(() => {
+  render(<HelloWorld />, document.getElementById('app'));
+});
